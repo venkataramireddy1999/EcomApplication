b43ea3122916ad200a394e9114419ca7c0726772
venkataramireddy1999 <your_github_email@example.com>
2026-04-23 12:52:19 +0530

    Fixed the issue, changed the User entity primary key primitive datatype to Wrapper class

--- src/main/java/com/nonsyncbobbal/EcomApplication/model/User.java
+++ src/main/java/com/nonsyncbobbal/EcomApplication/model/User.java
@@ -19,7 +19,7 @@ public class User {
 
     @Id
     @GeneratedValue( strategy = GenerationType.AUTO)
-    private int id;
+    private Integer id;
     private String firstName;
     private String lastName;
 }
